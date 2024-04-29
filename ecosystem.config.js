7554389e44c85a920b75ec2b0bdeb480307ff9a0
mmh0705 <bsi05607@gmail.com>
2024-04-29 16:34:44 +0900

    ecosystem 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'https://github.com/mmh0705/siin_next_shit.git',
       path : '/home/siin',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'cd /home/siin/source && source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options' : 'ForwardAgent=yes'
     }
