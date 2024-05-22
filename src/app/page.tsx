77085782f42549096c0cf1a5d1c8e79584475ff5
BAEK SIIN <siin@siin-laptop.localdomain>
2024-05-23 02:35:51 +0900

    배경화면 넣기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,41 @@
+"use client"
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  let [topBarOpacity, setTopBarOpacity] = useState<number>(0.5);
+  let [topBarFontColor, setTopBarFontColor] = useState<string>('black');
+
+  useEffect(() => {
+    window.addEventListener('scroll', function() {
+        //Get the number of pixels the document is currently scrolled vertically
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    
+        // Get the total scrollable height of the document
+        const documentHeight = document.documentElement.scrollHeight;
+        const windowHeight = window.innerHeight;
+        const totalScrollableHeight = documentHeight - windowHeight;
+    
+        // Calculate the scroll percentage
+        const scrollPercentage = (scrollPosition / totalScrollableHeight) * 100;
+    
+        // Check if scrolled all the way to the top
+        if (scrollPosition === 0) {
+          setTopBarOpacity(0.5);
+        }else{
+          setTopBarOpacity(1);
+        }
+    
+        // Check if scrolled down by 10% or more
+        if (scrollPercentage >= 10) {
+        }
+    })
+  }, []);
+  
+
+
+
   return (
     <div style={{backgroundColor:'green', height:'100vh'}}>
       <div style={{
@@ -13,17 +48,18 @@ export default function Home() {
         flexDirection:'row', 
         position:'fixed',
         width:'100%',
+        opacity: topBarOpacity,
         }}>
-        <div style={{flex:'1', color:'black'}}>로고</div>
-        <div style={{flex:'1', color:'black'}}>가위바위보</div>
-        <div style={{flex:'1', color:'black'}}>다빈치게임</div>
-        <div style={{flex:'1', color:'black'}}>미개발</div>
-        <div style={{flex:'1', color:'black'}}>미개발</div>
+        <div style={{flex:'1', color: topBarFontColor}}>로고</div>
+        <div style={{flex:'1', color: topBarFontColor}}>가위바위보</div>
+        <div style={{flex:'1', color: topBarFontColor}}>다빈치게임</div>
+        <div style={{flex:'1', color: topBarFontColor}}>미개발</div>
+        <div style={{flex:'1', color: topBarFontColor}}>미개발</div>
       </div>
 
 
-      <div style={{backgroundColor:'red', height:'500px', color:'black'}}>
-        asdfasdf
+      <div style={{height:'auto', color:'black'}}>
+        <img src="/one_cyberpunk.gif" style={{width:'100%'}}/>
       </div>
       <div style={{backgroundColor:'blue', height:'500px', color:'black'}}>
         asdfasdf
@@ -35,115 +71,5 @@ export default function Home() {
         asdfasdf
       </div>
     </div>
-
-    
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-    //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    //       이동주 명일방주 지금바로 다운로드 테스트&nbsp;
-    //       &nbsp;
-    //       <code className="font-mono font-bold">src/app/page.tsx</code>
-    //     </p>
-    //     <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-    //       <a
-    //         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{" "}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className="dark:invert"
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-    //     <Image
-    //       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className="mb-3 text-2xl font-semibold">
-    //         준호{" "}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className="m-0 max-w-[30ch] text-sm opacity-50">
-    //         Find in-depth information about Next.js features and API.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className="mb-3 text-2xl font-semibold">
-    //         동주{" "}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className="m-0 max-w-[30ch] text-sm opacity-50">
-    //         Learn about Next.js in an interactive course with&nbsp;quizzes!
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className="mb-3 text-2xl font-semibold">
-    //         시인{" "}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className="m-0 max-w-[30ch] text-sm opacity-50">
-    //         Explore starter templates for Next.js.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className="mb-3 text-2xl font-semibold">
-    //         레츠고{" "}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
   );
 }
